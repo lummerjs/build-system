2137374ac70bf78b2ca001b48b66e814780a75ed
lummerjs <PL@pb6a800c5.hrsmnt01.ap.so-net.ne.jp>
2016-02-29 22:34:38 +0100

    added default task and modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,10 @@
-gulpfile.js
+var gulp = require('gulp');
+var gutil = require('gulp-util')
+
+
+gulp.task('default', function() {
+    gutil.log("no default - use gulp <task>");
+    gutil.log(gutil.colors.green("gulp build"));
+    gutil.log(gutil.colors.green("gulp watch"));
+    gutil.log(gutil.colors.green("gulp dance"));
+});
